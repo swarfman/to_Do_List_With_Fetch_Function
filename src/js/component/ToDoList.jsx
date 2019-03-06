7a51f6b2b0dc93f2dacf2b66a313af017e90d66f
Sean <swarfman0509@gmail.com>
2019-03-06 19:22:36 +0000

    fifth commit

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -7,17 +7,24 @@ export class ToDoList extends React.Component {
 		super();
 		this.state = {
 			tasks: ["Laundry", "Groceries", "C.R.E.A.M."],
-			input: ""
+			input: "",
+			user: ""
 		};
 	}
 
 	componentDidMount = () => {
 		fetch("https://assets.breatheco.de/apis/fake/todos/user/swarfman")
 			.then(res => res.json())
-			.then(yoyo => {
-				let x = this.state.tasks;
-				this.setState({ tasks: yoyo });
-			});
+			.then(response => {
+				console.log("Success:", typeof response);
+				if (response === "") {
+					this.setState({ tasks: response });
+				} else {
+					this.setState({ tasks: [] });
+				}
+			})
+
+			.catch(error => console.error("Error:", error));
 	};
 	//adding the user's task into the ToDoList
 	enterFunction = e => {
@@ -85,49 +92,112 @@ export class ToDoList extends React.Component {
 		//removeUserCreatedArrayValue.push(tempObject);
 	};
 
+	addUser = a => {
+		if (a.key === "Enter") {
+			console.log(a);
+			var userInput1 = document.getElementById("userNameInput").value;
+			//console.log(userInput1);
+			var url =
+				"https://assets.breatheco.de/apis/fake/todos/user/" +
+				userInput1;
+
+			fetch(url, {
+				method: "POST", // or 'POST'
+				body: JSON.stringify([]), // data can be `string` or {object}!
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(res => {
+					return res.text();
+				})
+				.then(response => {
+					console.log("Success:", typeof response);
+					if (response === "") {
+						this.state.user;
+						fetch(url)
+							.then(res => res.json())
+							.then(pop => {
+								let x = this.state.tasks;
+								this.setState({ tasks: pop });
+							});
+					}
+				})
+
+				.catch(error => console.error("Error:", error));
+		}
+	};
+
 	render() {
 		return (
-			<div className="row justify-content-center">
-				<div className="centeredContent col-8 justify-content-center text-center">
-					<div className="title">
-						<h1>ToDo</h1>
+			<div>
+				<div className="row justify-content-center">
+					<div className="centeredContent col-8 justify-content-center text-center">
+						<div className="title">
+							<h1>ToDo</h1>
+						</div>
+						<div className="inputForm">
+							<input
+								id="listInput"
+								size="110"
+								onKeyPress={this.enterFunction}
+								placeholder="What do you want to do?"
+							/>
+						</div>
+						<ul className="Unordered List">
+							{this.state.tasks.map((elem, index) => {
+								return (
+									<li
+										className="d-flex justify-content-between"
+										key={index}>
+										{elem.label}
+										{elem.done && " 	Done!"}
+										<div className="d-flex flex-row-reverse">
+											<button
+												onClick={() =>
+													this.deleteFunction(index)
+												}
+												id="x">
+												<i className="far fa-trash-alt" />
+											</button>
+											<button
+												onClick={() =>
+													this.checkElementFunction(
+														index
+													)
+												}
+												id="x">
+												<i className="far fa-check-circle" />
+											</button>
+										</div>
+									</li>
+								);
+							})}
+						</ul>
 					</div>
-					<div className="inputForm">
-						<input
-							id="listInput"
-							size="110"
-							onKeyPress={this.enterFunction}
-							placeholder="What do you want to do?"
-						/>
+				</div>
+				<div className="row justify-content-center">
+					<div className="col-4 bg-dark mt-3 p-3">
+						<h6 className="text-white">Create New User Here</h6>
+						<div className="input-group mb-3">
+							<div className="input-group-prepend">
+								<span
+									className="input-group-text"
+									id="basic-addon1">
+									@
+								</span>
+							</div>
+							<input
+								type="text"
+								className="form-control"
+								id="userNameInput"
+								placeholder="Username"
+								aria-label="Username"
+								aria-describedby="basic-addon1"
+								onKeyPress={this.addUser}
+							/>
+						</div>
 					</div>
-					<ul className="Unordered List">
-						{this.state.tasks.map((elem, index) => {
-							return (
-								<li
-									className="d-flex justify-content-between"
-									key={index}>
-									{elem.label}
-									{elem.done && " 	Done!"}
-									<div className="d-flex flex-row-reverse">
-										<button
-											onClick={() =>
-												this.deleteFunction(index)
-											}
-											id="x">
-											<i className="far fa-trash-alt" />
-										</button>
-										<button
-											onClick={() =>
-												this.checkElementFunction(index)
-											}
-											id="x">
-											<i className="far fa-check-circle" />
-										</button>
-									</div>
-								</li>
-							);
-						})}
-					</ul>
 				</div>
 			</div>
 		);
